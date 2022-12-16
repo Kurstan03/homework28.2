9f6a00640bffeaf2b35708d67383be7d58349d93
Kurstan03 <kurstanerkinbaev2@gmail.com>
2022-12-16 10:31:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.IconUIResource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5,23,6,0,12,123,6,5,1,99,3,4,1,2);
+
+        Integer sum = list.stream().reduce(Integer::sum).get();
+        System.out.println("\nnsumma: " + sum);
+
+        Integer arif = Math.toIntExact(list.stream().count());
+        System.out.println("\narithmetic mean: " + ((double)sum / arif) + "\n");
+
+        Map<Integer, Integer> map = list.stream().collect(Collectors.toMap(x->x,x->1,Integer::sum));
+
+        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
+            System.out.println(m.getKey() + ": " + m.getValue() + " time repeated");
+        }
+    }
+}
